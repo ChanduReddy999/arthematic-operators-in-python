ceaceb6b33b280241a684f26106ee8cb4a0e6c89
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-09-28 23:21:57 +0530

    Add files via upload

---
+++ arthematic_operators_hr.py
@@ -0,0 +1,46 @@
+"""
+arthematic operations in python
+"""
+
+# method 1
+
+# a = int(input())
+# b = int(input())
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+
+
+# method 2
+
+# a=24
+# b=12
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+
+
+# method 3
+
+# num1 = input('Enter first number: ')  
+# num2 = input('Enter second number: ')  
+ 
+# sum = float(num1) + float(num2)       # Add two numbers 
+  
+# min = float(num1) - float(num2)       # Subtract two numbers
+  
+# mul = float(num1) * float(num2)       # Multiply two numbers
+  
+# div = float(num1) / float(num2)       # Divide two numbers
+  
+# print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))                # Display the sum
+  
+# print('The subtraction of {0} and {1} is {2}'.format(num1, num2, min))        # Display the subtraction
+  
+# print('The multiplication of {0} and {1} is {2}'.format(num1, num2, mul))     # Display the multiplication
+  
+# print('The division of {0} and {1} is {2}'.format(num1, num2, div))           # Display the division
